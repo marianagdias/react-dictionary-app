2864807d4b8593f9a3db3b15163fec8b409da808
marianagdias <marianagalantedias@gmail.com>
2022-05-22 10:47:45 +0100

    added onSubmit and onChange to store the value submitted

--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 
 export default function Search() {
+  const [keyword, setKeyword] = useState(null);
   function handleSearch(event) {
     event.preventDefault();
   }
+
+  function updateKeyword(event) {
+    setKeyword(event.target.value);
+  }
+
   return (
     <form className="Search" onSubmit={handleSearch}>
       <input type="search" autoFocus={true} placeholder="Write a word" />
-      <input type="submit" value="Search" />
+      <input type="submit" value="Search" onChange={updateKeyword} />
     </form>
   );
 }
