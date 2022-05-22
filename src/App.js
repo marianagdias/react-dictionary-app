90b0c1640323fab088faacfdfccf4676dafc4f6a
marianagdias <marianagalantedias@gmail.com>
2022-05-22 19:17:53 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ function App() {
         <h1>Dictionary</h1>
       </header>
       <Search />
+      <footer>
+        <small>
+          <a href="https://github.com/marianagdias/react-dictionary-app">
+            Open-source code
+          </a>{" "}
+          by Mariana Galante Dias
+        </small>
+      </footer>
     </div>
   );
 }
