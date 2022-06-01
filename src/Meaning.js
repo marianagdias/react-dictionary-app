79fe2faaae16848d95d4ec36625d4bc3423a2181
marianagdias <marianagalantedias@gmail.com>
2022-06-01 21:59:14 +0100

    continued styling app

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,38 +3,46 @@ import "./Meaning.css";
 
 export default function Meaning(props) {
   return (
-    <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      <p className="synonyms">
-        <strong>
-          Synonyms:{" "}
-          {props.meaning.synonyms.map(function (synonyms, index) {
-            return <span key={index}> {synonyms} </span>;
-          })}
-        </strong>
-      </p>
+    <div className="Meaning container">
+      <h3 className="row">{props.meaning.partOfSpeech}</h3>
+      <strong>
+        {" "}
+        <p className="row synonyms">
+          <div className="col-md-2">Synonyms:</div>
+          <div className="col-md-9">
+            {props.meaning.synonyms.map(function (synonyms, index) {
+              return (
+                <span className="synonym" key={index}>
+                  {" "}
+                  {synonyms}{" "}
+                </span>
+              );
+            })}
+          </div>
+        </p>
+      </strong>
       {props.meaning.definitions.map(function (definition, index) {
         if (definition.example) {
           return (
             <div className="content" key={index}>
               {/* <Synonyms synonyms={definition.synonyms} /> */}
-              <p>
-                <strong>Definition: </strong>
-                {definition.definition}
+              <p className="row">
+                <strong className="col-md-2">Definition: </strong>
+                <div className="col-md-9"> {definition.definition}</div>
               </p>
 
-              <p>
-                <strong>Example: </strong>
-                <em>{definition.example}</em>
+              <p className="row example">
+                <strong className="col-md-2"></strong>
+                <em className="col-md-9">{definition.example}</em>
               </p>
             </div>
           );
         } else {
           return (
             <div className="content" key={index}>
-              <p>
-                <strong>Definition: </strong>
-                {definition.definition}
+              <p className="row">
+                <strong className="col-md-2">Definition: </strong>
+                <div className="col-md-9"> {definition.definition}</div>
               </p>
             </div>
           );
