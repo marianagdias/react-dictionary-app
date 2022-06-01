08020bdf9fa2318afcb1305e85a086840418ef21
marianagdias <marianagalantedias@gmail.com>
2022-06-01 20:07:48 +0100

    added Phonetics

---
+++ src/Phonetics.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Phonetics(props) {
+  return (
+    <span className="Phonetics">
+      <h3>{props.phonetics.text}</h3>
+      <audio controls src={props.phonetics.audio}>
+        This element is not supported by your browser
+      </audio>
+    </span>
+  );
+}
